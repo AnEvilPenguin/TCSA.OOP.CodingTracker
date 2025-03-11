db3d5debaf1d36ed8aa27d8d4e8e4eecc0d432b0
Dave Lewis <siu06dl@gmail.com>
2025-03-11 19:22:35 +0000

    feat: initial database connection

--- Program.cs
+++ Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Data.SQLite;
+using Dapper;
+using TCSA.OOP.CodingTracker;
+
+await using var connection = new SQLiteConnection("Data Source=tracker.db");

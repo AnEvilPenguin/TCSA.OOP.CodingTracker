b5b6c494928eabb3c199176923b93dc0c637d404
Dave Lewis <siu06dl@gmail.com>
2025-03-11 21:10:49 +0000

    feat: initial database

---
+++ Controllers/ConnectionController.cs
@@ -0,0 +1,51 @@
+﻿using System.Data.SQLite;
+
+namespace TCSA.OOP.CodingTracker.Controllers;
+
+internal class ConnectionController (string connectionString)
+{
+    internal SQLiteConnection Initialize()
+    {
+        var connection = new SQLiteConnection(connectionString);
+     
+        connection.Open();
+        
+        InitProjectsTable(connection);
+        InitSessionsTable(connection);
+        
+        connection.Close();
+        
+        return connection;
+    }
+
+    private void InitSessionsTable(SQLiteConnection connection)
+    {
+        var query = @"
+            CREATE TABLE IF NOT EXISTS Sessions (
+                'Id' INTEGER PRIMARY KEY AUTOINCREMENT,
+                'Name' TEXT NOT NULL,
+                'Created' TEXT NOT NULL,
+                'Updated' TEXT NOT NULL,
+                'Started' TEXT NOT NULL,
+                'Finished' TEXT NOT NULL,
+                'Project' INTEGER NOT NULL,
+                FOREIGN KEY('Project') REFERENCES 'Projects'('Id') ON UPDATE CASCADE
+            );";
+        
+        new SQLiteCommand(query, connection).ExecuteNonQuery();
+    }
+
+    private void InitProjectsTable(SQLiteConnection connection)
+    {
+        var query = @"
+            CREATE TABLE IF NOT EXISTS Projects (
+                'Id' INTEGER PRIMARY KEY AUTOINCREMENT,
+                'Name' TEXT NOT NULL,
+                'Created' TEXT NOT NULL,
+                'Updated' TEXT NOT NULL,
+                'Repository' TEXT
+            );";
+        
+        new SQLiteCommand(query, connection).ExecuteNonQuery();
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,39 @@
 ﻿using System.Data.SQLite;
 using Dapper;
 using TCSA.OOP.CodingTracker;
+using TCSA.OOP.CodingTracker.Controllers;
 
-await using var connection = new SQLiteConnection("Data Source=tracker.db");
+var connectionController = new ConnectionController("Data Source=tracker.db");
+
+var connection = connectionController.Initialize();
+
+// Requirements
+// Log time spent coding
+    // Perhaps some shortcuts to say open a 'session' that can be closed off on completion later
+    // Cannot enter the duration, can only enter start and end times
+    // Specifically calls out a 'CalculateDuration' method
+// Need to use Spectre.Console for input and output
+// Separation of concerns
+// Prompt user with a specific format for date and time to be in and not allow any other format
+// Use a configuration file to contain database path and connection strings
+    // Perhaps guide the user through setting these if it doesn't exist yet?
+// Need to use Dapper ORM for data access
+    // Dapper does not support creating tables so these will have to be done using ADO.Net
+// Cannot use anonymous objects, have to use lists of sessions, etc.
+// Create and initialize a database if one is not present
+// CRUD operations
+// Must contain a README file
+// Use parameterized queries
+// Generate test data
+// Reporting
+
+// Allow tracking via stopwatch
+// Allow filtering their records per period
+// Allow ordering (in addition to filtering)
+// Reports for total and average coding session per period
+// Allow user to set goals and how far they are from their goal
+    // Daily average goal?
+    // Total goal
+        // How many hours a day to reach their goal
+        // How many days based on their average
+        

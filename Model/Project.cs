ac9680a5d9c8c12b39925a5250464d57c53d9169
Dave Lewis <siu06dl@gmail.com>
2025-03-13 17:11:51 +0000

    feat: initial models

---
+++ Model/Project.cs
@@ -0,0 +1,10 @@
+﻿namespace TCSA.OOP.CodingTracker.Model;
+
+internal class Project
+{
+    internal int Id { get; set; }
+    internal required string Name { get; set; }
+    internal DateTime Created { get; set; }
+    internal DateTime Updated { get; set; }
+    internal string? Repository { get; set; }
+}
---
+++ Model/Session.cs
@@ -0,0 +1,12 @@
+﻿namespace TCSA.OOP.CodingTracker.Model;
+
+internal class Session
+{
+    internal int Id { get; set; }
+    internal required string Name { get; set; }
+    internal DateTime Created { get; set; }
+    internal DateTime Updated { get; set; }
+    internal DateTime Started { get; set; }
+    internal DateTime Finished { get; set; }
+    internal required Project Project { get; set; }
+}

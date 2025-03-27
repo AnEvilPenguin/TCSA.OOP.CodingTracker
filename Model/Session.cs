84f626d75ede572fc64ecb0010c408cc302be2e1
Dave Lewis <siu06dl@gmail.com>
2025-03-27 18:50:50 +0000

    docs: remove old decision

--- Model/Session.cs
+++ Model/Session.cs
@@ -2,8 +2,6 @@
 
 internal class Session
 {
-    // Do we actually want to have a bool for open sessions?
-    // Might make things easier?
     internal int Id { get; set; }
     internal required string Name { get; set; }
     internal DateTime Created { get; set; } = DateTime.UtcNow;

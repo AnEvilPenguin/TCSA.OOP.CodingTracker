f25446be2396e6d86c2927a5e9721a82fa8fde70
Dave Lewis <siu06dl@gmail.com>
2025-03-28 17:28:13 +0000

    doc: clean up todo list

--- Program.cs
+++ Program.cs
@@ -24,26 +24,12 @@ return new MainMenu(sessionController).Run(args);
 
 
 // Requirements
-// Log time spent coding
-// Log time spent coding
-    // Perhaps some shortcuts to say open a 'session' that can be closed off on completion later
-    // Cannot enter the duration, can only enter start and end times
-    // Specifically calls out a 'CalculateDuration' method
-// Need to use Spectre.Console for input and output
-// Separation of concerns
-// Prompt user with a specific format for date and time to be in and not allow any other format
+
 // Take command line if possible
     // eg gh repo create
     // if args.length > 0 parse them and run otherwise prompty prompt
-// Use a configuration file to contain database path and connection strings
-    // Perhaps guide the user through setting these if it doesn't exist yet?
-// Need to use Dapper ORM for data access
-    // Dapper does not support creating tables so these will have to be done using ADO.Net
-// Cannot use anonymous objects, have to use lists of sessions, etc.
-// Create and initialize a database if one is not present
-// CRUD operations
+
 // Must contain a README file
-// Use parameterized queries
 // Generate test data
 // Reporting
 
